56cd9ee1417981af88c77724d19d3391cfec5a1f
cavalcante-dev <leandrocavalcante2504@gmail.com>
2025-03-25 20:24:49 -0300

    Supresswarning para sair a sugestão do try with do codigo

--- Atv8_Financiamentos.java
+++ Atv8_Financiamentos.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Atv8_Financiamentos {
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double salario, setentaPorCento, salarioReferencia, prestacao;
