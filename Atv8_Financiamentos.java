2a1f8d51ae5dedff831b551f40baf6c6349437ff
cavalcante-html <leandrocavalcante2504@gmail.com>
2025-03-28 13:09:04 -0300

    Teste

--- Atv8_Financiamentos.java
+++ Atv8_Financiamentos.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class Atv8_Financiamentos {
-    @SuppressWarnings("ConvertToTryWithResources")
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double salario, setentaPorCento, salarioReferencia, prestacao;
